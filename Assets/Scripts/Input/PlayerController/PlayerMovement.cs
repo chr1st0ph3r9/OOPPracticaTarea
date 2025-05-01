fac256f26bb276fce817496d1bc585c6b46db2fb
Christopher Hayling <christopherhayling9@gmail.com>
2025-05-01 11:15:52 -0500

    movimiento jugador 0.5

---
+++ Assets/Scripts/Input/PlayerController/PlayerMovement.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public MainInputMovementSO inputReader;
+
+    //para movimiento
+    public Vector3 direccionMovimiento;
+    public float velocidadMovimiento = 10;
+    public Rigidbody rb;
+
+    //para camara
+    public Transform camara;
+
+
+    public int anguloMaximoRotacion = 90;
+    public int anguloMinimoRotacion = -90;
+
+    public float rotacionY;
+    public float sensibilidadCamara = 5f;
+
+
+
+
+    private void MovimientoCamara(Vector2 direccion)
+    {
+        float anguloY = sensibilidadCamara * direccion.y*Time.deltaTime;
+        float anguloX = sensibilidadCamara * direccion.x * Time.deltaTime;
+        float anguloXArribaAbajo =Mathf.Clamp(anguloY, anguloMinimoRotacion, anguloMaximoRotacion) ;
+
+        rotacionY -= anguloXArribaAbajo;
+
+        camara.localRotation = Quaternion.Euler(rotacionY, 0,0);
+
+        transform.Rotate(Vector3.up * anguloX);
+
+    }
+
+    private void Awake()
+    {
+        inputReader.eventoMoverHorizontal += movimientoBase;
+        inputReader.eventoMirar += MovimientoCamara;
+
+
+
+    }
+    private void OnDisable()
+    {
+        inputReader.eventoMoverHorizontal -= movimientoBase;
+        inputReader.eventoMirar -= MovimientoCamara;
+
+
+    }
+    private void FixedUpdate()
+    {
+        logicaMovimiento();
+    }
+
+
+
+    //movimiento de jugador
+    private void movimientoBase(Vector2 direccionDeMovimiento)
+    {
+        Vector3 forward = Vector3.ProjectOnPlane(Vector3.forward, Vector3.up).normalized;
+        direccionMovimiento = new Vector3(direccionDeMovimiento.x ,0,direccionDeMovimiento.y);
+    }
+
+    private void logicaMovimiento()
+    {
+        //rb.linearVelocity
+        rb.linearVelocity=(direccionMovimiento*velocidadMovimiento)*Time.fixedDeltaTime;
+    }
+
+
+    //movimiento de camara
+
+
+}
---
+++ Assets/Scripts/Input/ScriptableObject/MainInputMovementSO.cs
@@ -0,0 +1,120 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[CreateAssetMenu(fileName ="nuevo InputReaderSO", menuName ="InputReaderSO", order =0)]
+public class MainInputMovementSO : ScriptableObject,InputSystem_Actions.IPlayerActions
+{
+
+
+
+
+    private InputSystem_Actions inputActions;
+
+    
+
+    public delegate void inputAxisAction(Vector2 movimiento);
+
+    public event inputAxisAction eventoMoverHorizontal;
+    public event inputAxisAction eventoMirar;
+
+    public delegate void inputAtaqueAction();
+
+    public event inputAtaqueAction eventoAtaqueIniciado;
+    public event inputAtaqueAction eventoAtaqueCompletado;
+    public event inputAtaqueAction eventoAtaqueCancelado;
+
+
+
+
+
+
+
+
+
+    private void OnEnable()
+    {
+        inputActions = new InputSystem_Actions();
+
+        inputActions.Player.Enable();
+
+        inputActions.Player.AddCallbacks(this);
+
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Player.Disable();
+    }
+
+
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        switch (context.phase)
+        {
+            case InputActionPhase.Started:
+
+                Debug.Log("empezo ataque");
+                eventoAtaqueIniciado?.Invoke();
+
+                break;
+
+            case InputActionPhase.Performed:
+
+                Debug.Log("completo ataque");
+                eventoAtaqueCompletado?.Invoke();
+
+                break;
+
+            case InputActionPhase.Canceled:
+
+                Debug.Log("cancelo ataque");
+                eventoAtaqueCancelado?.Invoke();
+
+                break;
+
+
+        }
+    }
+
+    public void OnCrouch(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        eventoMirar?.Invoke(context.ReadValue<Vector2>());
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        eventoMoverHorizontal?.Invoke(context.ReadValue<Vector2>());
+    }
+
+    public void OnNext(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPrevious(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnSprint(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
+    }
+
+
+}
